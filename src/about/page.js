1d99524a68b14027825e35d0acb6d45d1c6f8e17
SodusMedia <sodussp@gmail.com>
2025-09-24 13:36:54 +0100

    Refactord app pages into src directory

--- src/about/page.js
+++ src/about/page.js
@@ -1,10 +1,14 @@
 // src/app/about/page.js
 import Contact from '@/components/Contact';
+import NavBars from '@/components/NavBars';
 
 
 export default function AboutPage() {
   return (
     <main>
+   <>
+      <NavBars />
+    </>
       <h1>About Fransusnest</h1>
       <p>
         We are a consulting and development firm focused on innovation, excellence, and sustainable growth in Nigeria.
